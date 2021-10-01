becec9163994d00ea37fc1bb66b00a13ae2d1ac4
saitonaoki <reisa.naoki@icloud.com>
2021-10-01 22:42:05 +0900

    firstcommit

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+# DIC_task_list_app
---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,8 @@
+class TaskController < ApplicationController
+  def new
+    @task = Task.new
+  end
+  def index
+    @tasks = Task.all
+  end
+end
